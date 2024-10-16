558e4b91136a5b5393e3169e44ae9d5b8fa2f971
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2024-10-15 19:07:58 -0500

    added man page, install script for UNIX systems, and .gitignore

---
+++ spotify.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Installation script for spotify command
+
+MAN_DIR="/usr/local/share/man/man1"
+MAN_PAGE="spotify.1"
+
+if [ "$(id -u)" -ne 0 ]; then 
+    echo "This script must be run as root/administrator. Please use sudo."
+    exit 1
+fi
+
+mkdir -p "$MAN_DIR"
+if [ $? -ne 0 ]; then
+    echo "Failed to install spotify command. Please try again."
+    exit 1
+fi
+cp "$MAN_PAGE" "$MAN_DIR"
+if [ $? -ne 0 ]; then
+    echo "Failed to install spotify command. Please try again."
+    exit 1
+fi
+
+echo "Installation complete. Please use 'man spotify' or 'spotify --help' for more information."
