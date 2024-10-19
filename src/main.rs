d293275ecb933c86ba3960ac68a2746c681e8b61
Leaves333 <canchenli@gmail.com>
2024-10-18 20:22:36 -0500

    rspotify

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
-pub mod options;
+use rspotify::{model::AlbumId, prelude::*, ClientCredsSpotify, Credentials};
 
-extern crate spotify;
-use spotify::Spotify;
+#[tokio::main]
+async fn main() {
+    // You can use any logger for debugging.
+    //env_logger::init();
 
-use options::handle_args;
-use std::env;
+    // Set RSPOTIFY_CLIENT_ID and RSPOTIFY_CLIENT_SECRET in an .env file (after
+    // enabling the `env-file` feature) or export them manually:
+    //
+    // export RSPOTIFY_CLIENT_ID="your client_id"
+    // export RSPOTIFY_CLIENT_SECRET="secret"
+    // export RSPOTIFY_REDIRECT_URI="your redirect uri"
+    //
+    // These will then be read with `from_env`.
+    //
+    // Otherwise, set client_id and client_secret explictly:
+    //
+    // ```
+    // let creds = Credentials {
+    //     id: "this-is-my-client-id".to_string(),
+    //     secret: Some("this-is-my-client-secret".to_string())
+    // };
+    // ```
+    let creds = Credentials::from_env().unwrap();
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    handle_args(args);
-}
+    let spotify = ClientCredsSpotify::new(creds);
 
-fn welcome() {
-    println!("usage: spotify [-v | --version] [-h | --help] [new]");
+    // Obtaining the access token. Requires to be mutable because the internal
+    // token will be modified. We don't need OAuth for this specific endpoint,
+    // so `...` is used instead of `prompt_for_user_token`.
+    spotify.request_token().await.unwrap();
+
+    // Running the requests
+    let birdy_uri = AlbumId::from_uri("spotify:album:0sNOF9WDwhWunNAHPD3Baj").unwrap();
+    let albums = spotify.album(birdy_uri, None).await;
+
+    println!("Response: {albums:#?}");
 }
 
