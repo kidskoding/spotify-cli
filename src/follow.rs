bcf1f202ffe31deb58af63df6e7b27d832a250d7
Leaves333 <canchenli@gmail.com>
2024-12-11 14:59:05 -0600

    refactor follow.rs

--- src/follow.rs
+++ src/follow.rs
@@ -3,6 +3,7 @@ use rspotify::model::{ArtistId, FullArtist};
 
 use crate::auth;
 
+// helper function to get extra information about an artist from an id
 pub async fn parse_artist_id(artist_id: &str) -> FullArtist {
     let spotify = auth::spotify_from_token();
     let result = ArtistId::from_id_or_uri(artist_id).expect("invalid artist id!");
@@ -13,45 +14,40 @@ pub async fn parse_artist_id(artist_id: &str) -> FullArtist {
 }
 
 pub async fn follow(id: &str) {
-    let spotify = auth::spotify_from_token();
-
+    // user_follow_artists takes an iterator...
+    // so we put the one artist we want to follow into a vec
     let mut artists: Vec<ArtistId> = Vec::new();
     let artist_id = ArtistId::from_id_or_uri(id).expect("invalid artist id!");
-    let artist = parse_artist_id(id).await;
     artists.push(artist_id.clone());
-    let result = spotify.user_follow_artists(artists).await;
 
-    match result {
-        Ok(_) => {
-            println!(
-                "successfully followed {} with an id of {}",
-                artist.name, artist_id
-            );
-        }
-        Err(x) => {
-            println!("{}", x);
-        }
-    }
+    let spotify = auth::spotify_from_token();
+    let _ = spotify
+        .user_follow_artists(artists)
+        .await
+        .expect("couldn't follow artist!");
+
+    let artist = parse_artist_id(id).await;
+    println!(
+        "successfully followed {} with an id of {}",
+        artist.name, artist_id
+    );
 }
 
 pub async fn unfollow(id: &str) {
-    let spotify = auth::spotify_from_token();
-
+    // similar logic for unfollowing artists
     let mut artists: Vec<ArtistId> = Vec::new();
     let artist_id = ArtistId::from_id_or_uri(id).expect("invalid artist id!");
-    let artist = parse_artist_id(id).await;
     artists.push(artist_id.clone());
-    let result = spotify.user_unfollow_artists(artists).await;
 
-    match result {
-        Ok(_) => {
-            println!(
-                "successfully unfollowed {} with an id of {}",
-                artist.name, artist_id
-            );
-        }
-        Err(x) => {
-            println!("{}", x);
-        }
-    }
+    let spotify = auth::spotify_from_token();
+    let _ = spotify
+        .user_unfollow_artists(artists)
+        .await
+        .expect("couldn't unfollow artist!");
+
+    let artist = parse_artist_id(id).await;
+    println!(
+        "successfully unfollowed {} with an id of {}",
+        artist.name, artist_id
+    );
 }
